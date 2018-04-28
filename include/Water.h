11daa378f6617f7a439d383e903b6f8ff90d857a
stephen321 <stephen.enniis@gmail.com>
2018-04-28 22:07:45 +0100

    added basic blur shader and water particles

--- include/Water.h
+++ include/Water.h
@@ -14,16 +14,43 @@ public:
 	void splash(float position, float strength);
 private:
 	void createSines();
-	float overlapSines(float x);
+	float overlapSines(float x) const;
 	struct Spring {
 		sf::Vector2f position; 
 		sf::Vector2f velocity;
 		sf::Vector2f acceleration;
 	};
+
+	//////////////////////////////////////////////////////////////////
+	//https://gamedev.stackexchange.com/questions/44547/how-do-i-create-2d-water-with-dynamic-waves
+	//////////////////////////////////////////////////////////////////
+	// A phase difference to apply to each sine
+	float m_offset;
+	// Amounts by which a particular sine is offset
+	std::vector<float> m_sineOffsets;
+	// Amounts by which a particular sine is amplified
+	std::vector<float> m_sineAmplitudes;
+	// Amounts by which a particular sine is stretched
+	std::vector<float> m_sineStretches;
+	// Amounts by which a particular sine's offset is multiplied
+	std::vector<float> m_offsetStretches;
+	// Set each sine's values to a reasonable random value
+	//////////////////////////////////////////////////////////////////
+
 	sf::Clock m_timer;
 	se::EntityInstance* m_entity;
 	const sf::RenderWindow* window;
 	mutable sf::VertexArray m_vertices;
 	float m_level;
 	Spring m_springs[WATER_SPRINGS_COUNT];
+
+	//todo: first particle attempts uses sf::Sprites, switch to quads + vertex array?
+	struct Particle {
+		sf::Vector2f position;
+		sf::Vector2f velocity;
+		sf::Sprite sprite;
+		bool alive = true;
+	};
+	sf::Shader* m_blurShader;
+	std::vector<Particle> m_particles;
 };
